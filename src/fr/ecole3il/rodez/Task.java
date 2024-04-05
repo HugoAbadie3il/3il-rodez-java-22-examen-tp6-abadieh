19a1ff1c60264e60584be641690a27b4b1fcb3cd
hugo.abadie <abadieh@3il.fr>
2024-04-05 14:57:45 +0200

    [TEST] task, added javadoc

--- src/fr/ecole3il/rodez/Task.java
+++ src/fr/ecole3il/rodez/Task.java
@@ -1,79 +1,148 @@
 package fr.ecole3il.rodez;
 
+import java.util.Date;
 import java.util.Objects;
 
+/**
+ * Class for representing a Task
+ */
 public class Task {
 
 	private int id;
 	private String titre;
 	private String description;
 	private int priority;
-	private String dateEcheance;
+	private Date dateEcheance;
 	private String status;
 	
-	public Task(String status, int id, String titre, String description, int priority, String date)
+	/**
+	 * Constructor for a task
+	 * @param status the status of the task
+	 * @param id the id of the task
+	 * @param titre the title of the task
+	 * @param description the description of the task
+	 * @param priority the priority of the task
+	 * @param date the deadline for the task
+	 */
+	public Task(String status, int id, String titre, String description, int priority, Date dateEcheance)
 	{
 		this.status = status;
 		this.id = id;
 		this.titre = titre;
 		this.description = description;
 		this.priority = priority;
-		this.dateEcheance = date;
+		this.dateEcheance = dateEcheance;
 	}
 
+	/**
+	 * Getter for id
+	 * @return the id of the task
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Setter the id of the task
+	 * @param id the new id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Getter of the title of the task
+	 * @return the title of the task
+	 */
 	public String getTitre() {
 		return titre;
 	}
 
+	/**
+	 * Setter for the title of the class
+	 * @param titre the new title of the task
+	 */
 	public void setTitre(String titre) {
 		this.titre = titre;
 	}
 
+	/**
+	 * Getter of the description
+	 * @return the description of the task
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Setter of the description of the task
+	 * @param description the new description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+	/**
+	 * Getter of the priority of the task
+	 * @return the priority of the task
+	 */
 	public int getPriority() {
 		return priority;
 	}
 
+	/**
+	 * Setter of the priority of the task
+	 * @param priority the new priority
+	 */
 	public void setPriority(int priority) {
 		this.priority = priority;
 	}
 
-	public String getDateEcheance() {
+	/**
+	 * Getter of the deadline of the task
+	 * @return the deadline of the task
+	 */
+	public Date getDateEcheance() {
 		return dateEcheance;
 	}
 
-	public void setDateEcheance(String dateEcheance) {
+	/**
+	 * Setter of the deadline of the task
+	 * @param dateEcheance the deadline of the task
+	 */
+	public void setDateEcheance(Date dateEcheance) {
 		this.dateEcheance = dateEcheance;
 	}
 	
+	/**
+	 * Getter of the status
+	 * @return the status of the task
+	 */
 	public String getStatus() {
 		return status;
 	}
 	
+	/**
+	 * Setter of the status of the task
+	 * @param status the new status
+	 */
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
+	/**
+	 * HashCode method
+	 * @return the hash code of the task
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(dateEcheance, description, id, priority, status, titre);
 	}
 
+	/**
+	 * Equals method
+	 * @return true if all attributes are the same value, false elsewhere
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
---
+++ test/fr/ecole3il/rodez/TestTask.java
@@ -0,0 +1,75 @@
+package fr.ecole3il.rodez;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
+
+/**
+ * Class for testing the task class
+ */
+class TestTask {
+	// The default task
+	private Task task;
+	
+	/**
+	 * Set up the default task
+	 */
+	@BeforeEach
+	void setUp() {
+		@SuppressWarnings("deprecation")
+		Date echeance = new Date(2024, 5, 4);
+		task = new Task("TODO", 0, "test", "Tester les classes", 3, echeance);
+	}
+
+	/**
+	 * Test if a basic task is well created
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	void BasicCase() {
+		Assertions.assertEquals("TODO", task.getStatus());
+		Assertions.assertEquals(0, task.getId());
+		Assertions.assertEquals(3, task.getPriority());
+		Assertions.assertEquals("test", task.getTitre());
+		Assertions.assertEquals("Tester les classes", task.getDescription());
+		Assertions.assertEquals(new Date(2024, 5, 4), task.getDateEcheance());
+		
+	}
+	
+	/**
+	 * Checks the method equals return true for same task
+	 */
+	@Test
+	void SameTask() {
+		@SuppressWarnings("deprecation")
+		Date echeance = new Date(2024, 5, 4);
+		Task sameTask = new Task("TODO", 0, "test", "Tester les classes", 3, echeance);
+		Assertions.assertTrue(task.equals(sameTask));
+	}
+	
+	/**
+	 * Checks the method equals return false for same task but different id
+	 */
+	@Test
+	void SameTaskDifferentId() {
+		@SuppressWarnings("deprecation")
+		Date echeance = new Date(2024, 5, 4);
+		Task sameTask = new Task("TODO", 1, "test", "Tester les classes", 3, echeance);
+		Assertions.assertTrue(task.equals(sameTask));
+	}
+	
+	/**
+	 * Checks the method equals return false for different task but same id
+	 */
+	@Test
+	void DifferentTaskSameId() {
+		@SuppressWarnings("deprecation")
+		Date echeance = new Date(2024, 5, 5);
+		Task sameTask = new Task("Doing", 0, "dormir", "Recharger les batteries", 1, echeance);
+		Assertions.assertTrue(task.equals(sameTask));
+	}
+
+}
